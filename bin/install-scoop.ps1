4f62fbae5abecb119d26dcf1b22deab2b98f1b65
CornerSyrup <87284079+CornerSyrup@users.noreply.github.com>
2021-12-19 05:26:07 +0000

    script(scoop): Add extras bucket
    - Add extras bucket
    - Reorder install commands

--- bin/install-scoop.ps1
+++ bin/install-scoop.ps1
@@ -8,23 +8,28 @@ scoop install 7zip
 scoop install 7zip -g
 
 scoop install adb -g
-scoop install android-sdk -g
 scoop install arduino -g
 scoop install cmder-full -g
 scoop install dart -g
 scoop install dotnet-sdk -g
 scoop install draw.ioo -g
 scoop install flutter -g
-scoop install github -g
 scoop install go -g
 scoop install make -g
 scoop install maven -g
 scoop install notionjs-lts -g
+scoop install sudo -g
+scoop install vim -g
+
+scoop bucket add extras
+scoop install android-sdk -g
+scoop install github -g
+scoop install libreoffice-stable -g
+scoop install notion -g
 scoop install obs-studio -g
 scoop install postman -g
 scoop install signal -g
-scoop install sudo -g
-scoop install vim -g
+scoop install vcredist2019 -g
 scoop install vscode -g
 
 scoop bucket add nerd-fonts
