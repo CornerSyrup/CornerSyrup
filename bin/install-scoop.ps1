dcff58779a12bc23b420667e09116aff25d0eb65
CornerSyrup <87284079+CornerSyrup@users.noreply.github.com>
2022-04-23 12:32:50 +0000

    refactor(scoop): Modularize and Functional-ize

--- bin/install-scoop.ps1
+++ bin/install-scoop.ps1
@@ -1,63 +1,86 @@
+[CmdletBinding()]
+param (
+    [switch] $extras = $false,
+    [switch] $language = $false,
+    [switch] $devEnv = $false,
+    [switch] $haskell = $false,
+    [switch] $java = $false,
+    [switch] $php = $false,
+    [switch] $optionals = $false
+)
+
+function Install-Package {
+    param (
+        [string]$name,
+        [switch]$global = $false
+    )
+
+    if ($global) {
+        scoop install $name -g
+    }
+    else {
+        scoop install $name
+    }
+}
+
+function Invoke-WithBucket {
+    param (
+        [string] $bucket,
+        [scriptblock[]] $block
+    )
+    scoop bucket add $bucket
+    $block | ForEach-Object { $_.Invoke() }
+    scoop bucket rm $bucket
+}
+
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
-scoop install git -g
-
-scoop install 7zip
-scoop install 7zip -g
-
-scoop install cmder-full -g
-scoop install dotnet-sdk -g
-scoop install draw.io -g
-scoop install ffmpeg -g
-scoop install go -g
-scoop install make -g
-scoop install mingw-winlibs -g # Haskell need its gcc
-scoop install nodejs-lts -g
-scoop install python -g
-scoop install sudo -g
-scoop install vim -g
-
+# Basic
 scoop bucket add extras
-scoop install github -g
-scoop install libreoffice-stable -g
-scoop install notion -g
-scoop install obs-studio -g
-scoop install postman -g
-scoop install signal -g
-scoop install vcredist2019 -g
-scoop install vscode -g
-
-# Haskell
-scoop install haskell@8.10.7 -g
-scoop install haskell-cabal -g
-scoop install stack -g
-
-
-# Java
-scoop bucket add java
-scoop install kotlin -g
-scoop install maven -g
-scoop install openjdk8-redhat -g
-scoop install openjdk11 -g
-
-# Adnroid
-scoop install android-sdk -g
-scoop install adb -g
-
-# JetBrain
-scoop bucket add jetbrains
-scoop install IntelliJ-IDEA -g
-scoop install DotPeek -g
-scoop install YouTrack -g
-
-# PHP
-scoop bucket add php
-scoop install php -g
-scoop install php-xdebug -g
-scoop install composer -g
+@("git", "aria2", "7zip") | ForEach-Object { Install-Package -name $_ -global }
+@("7zip") | ForEach-Object { Install-Package -name $_ -global }
 
 # Fonts
-scoop bucket add nerd-fonts
-scoop install FiraCode-NF -g
+Invoke-WithBucket -bucket "nerd-fonts" { Install-Package -name "FiraCode-NF" -global }
+
+# Tools
+@("cmder-full", "ffmpeg", "mingw-winlibs", "sudo", "vim", "vscode") | ForEach-Object { Install-Package -name $_ -global }
+@("signal") | ForEach-Object { Install-Package -name $_ }
+
+# Extras
+if ($extras) {
+    @("draw.io", "notion", "libreoffice-stable", "obs-studio") | ForEach-Object { Install-Package -name $_ -global }
+}
+
+# Languages
+if ($language) {
+    @("dotnet-sdk", "go", "nodejs-lts", "python") | ForEach-Object { Install-Package -name $_ -global }
+}
+
+# Dev Env
+if ($devEnv) {
+    @("make", "github", "postman") | ForEach-Object { Install-Package -name $_ -global }
+}
+
+# Haskell
+if ($haskell) {
+    @("haskell@8.10.7", "haskell-cabal", "stack") | ForEach-Object { Install-Package -name $_ -global }
+}
+
+# Java
+if ($java) {
+    scoop bucket add java
+    @("kotlin", "maven", "openjdk8-redhat", "openjdk11") | ForEach-Object { Install-Package -name $_ -global }
+}
+
+# PHP
+if ($php) {
+    @("php", "php-xdebug", "composer") | ForEach-Object { Install-Package -name $_ -global }
+}
+
+# Optionals
+if ($optionals) {
+    @("vcredist2019") | ForEach-Object { Install-Package -name $_ -global }
+}
