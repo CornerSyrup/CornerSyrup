c5a507cac05d1b7a2098446be3fd6f92c8479dff
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-11-13 21:06:27 +0000

    feat(npm): :wrench: Add npm global install scripts

--- cmd/npm-pkg.sh
+++ cmd/npm-pkg.sh
@@ -1,1 +1,15 @@
-npm install apidoc --global
+# Documentation tool
+npm install --global apidoc
+
+# Dev Env
+npm install --global nodemon
+
+# Test
+npm install --global jest
+
+# TypeScript
+npm install --global typescript ts-node
+npm install --global @types/node
+
+# Purescript
+npm install --global purescript purty spago
